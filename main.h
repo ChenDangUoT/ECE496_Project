39f2bbaf9a5db7a32ddb420249b94700ab1e6a2f
Chen Dang <chen.dang@mail.utoronto.ca>
2018-02-12 13:14:28 -0500

    Changes: Trying to add main.cpp and main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+
+// our final destination is to input vector<bool> and output vector<pair<float,float>> which translates the bits to a priori probability
+
+using std::vector;
+using std::pair;
+
+
